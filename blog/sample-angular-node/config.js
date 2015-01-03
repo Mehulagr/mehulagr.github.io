a3f6b7cae514e22a82a42e2badf3c44b7f30ec6c
Mehul Agrawal <agrawal.mehul92@gmail.com>
2015-01-04 00:44:15 +0530

    sample-angular-node

---
+++ blog/sample-angular-node/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+	twitter: {
+  	consumer_key: 'ivI1ju6PGfoOjTZEym5AQSMvc',
+		consumer_secret: 'TM8EhYlOfYShg6ajMphyTzJvNI2mtnxgTUtW7Ch8XnMARwpzA5',
+		access_token: '27197728-kgVddiLVsE8Z91nztB5p3B6q5P2oOdDv4GtOuzcS8',
+		access_token_secret: 'cbA5XYUicvr8uNpBRVjNT6VYpbMgrXeDmTWHmrKJI4uIZ'
+	}
+
+};
